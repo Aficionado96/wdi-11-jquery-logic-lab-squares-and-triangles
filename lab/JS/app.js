6b74ae8dad466938385b388951d9150d44e688b6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-03-24 15:09:14 -0500

    Got the checkerBoard working

--- lab/JS/app.js
+++ lab/JS/app.js
@@ -10,30 +10,91 @@ black = 'rgb(0,0,0)';
 
 
 
-const generateDivs = (color)=>{
+const generateDivs = (color,checkerboardId)=>{
 	let $div = $('<div>').css({
 		'background-color':color,
 		'height':'30px',
 		'width':'30px'
 	}).addClass('line')
 	
-	$div.appendTo($('body'))
+	$div.appendTo($(checkerboardId))
 
 	
 }
 
 for(let i = 0; i < 8; i ++){
-
+	 $('<div>').appendTo($('#checkerboard2'))
 	//creates squares
 	for(let j = 1; j <= 4; j++){
-	generateDivs(red);
-	generateDivs(black);
+		generateDivs(red,'#checkerboard2');
+		generateDivs(black,'#checkerboard2');
 	
 	}
+
 	//creates rows
-	$('<div>').appendTo($('body'))
+	// $row
 	
 }
+
+
+
+
+
+
+//2
+
+//we are going to use some methods and variables from previous problem
+
+
+
+
+
+//GENErates row starting w red
+const generateRedToBlack=()=>{ 
+	for(let i = 1; i <= 4; i ++){
+		generateDivs(red,'#checkerboard');
+		generateDivs(black,'#checkerboard');
+	}
+
+
+}	
+
+//gens a row starting w black
+const generateBlackToRed = ()=>{
+	for(let i = 1; i <= 4; i ++){
+		generateDivs(black,'#checkerboard');	
+		generateDivs(red,'#checkerboard');
+		
+	}
+}
+
+
+// 4 times: 1 red-black, 1 black-red
+for(let i = 0; i < 4; i ++){
+	
+	generateRedToBlack();
+
+	$('<div>').addClass('noSpace').appendTo($('#checkerboard'));
+
+	generateBlackToRed();
+
+	$('<div>').addClass('noSpace').appendTo($('#checkerboard'))
+}
+
+
+
+
+
+//3
+//Bonus
+
+
+
+
+
+
+
+
 
 
 
